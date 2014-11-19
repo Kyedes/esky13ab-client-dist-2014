c4f15cf06cc71a1aeac2eace7a16ea7a11777e6a
Kyedes <esky13ab@student.cbs.dk>
2014-11-19 14:30:35 +0100

    Project Start, made the connection class and added object classes.

---
+++ Client for dist 2014/src/ServerConnection.java
@@ -0,0 +1,34 @@
+import java.net.Socket;
+
+/**
+ * This class makes it possible to communicate with the server
+ * @author Esben
+ *
+ */
+public class ServerConnection {
+	
+//	private String jsonOut;
+	private String jsonIn;
+	private byte[] bAOut = new byte[500000];
+	private byte[] bAIn = new byte[500000];
+	private Encryption crypt = new Encryption();
+	
+	/**
+	 * When executing the server connection it takes a Json String and encrypt it before sending it to the server.
+	 * The answer from the server is de-crypted and returned as a Json String.
+	 * @param jsonOut
+	 * @return
+	 * @throws Exception
+	 */
+	public String execute(String jsonOut) throws Exception{
+		Socket connectionSocket = new Socket("localhost", 8888);
+		bAOut = crypt.encrypt(jsonOut);
+		connectionSocket.getOutputStream().write(bAOut);
+		int i = connectionSocket.getInputStream().read(bAIn);
+		System.out.print(i);//To do somthing with i, not nessesary
+		connectionSocket.close();
+		jsonIn = crypt.decrypt(bAIn);
+		
+		return jsonIn;
+	}
+}
